5b246124a6c38e0288cb4b34d336f5009cbada63
Ghatan55 <ghatanhardanni@gmail.com>
2025-06-26 10:18:48 +0700

    Menambahkan step 3 untuk Fungsi insert pada BinaryTree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -35,5 +35,9 @@ public:
         Node *newNode = new Node();
         // Step 2: Assign value to the data field of new node
         newNode->info = x;
+
+        // Step 3: Make the left and right child of the new node point ot NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
     }
 };
