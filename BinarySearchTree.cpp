b2d7b9973c0b99f970d4a2d7066fa5f0528f38e6
Ghatan55 <ghatanhardanni@gmail.com>
2025-06-26 10:06:34 +0700

    Membuat class node dengan akses public

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the node Class
+    Node()
+    {
+        leftchild = nullptr;  // Initialize left child to null
+        rightchild = nullptr; // Initialize right child to null
+    }
+};
